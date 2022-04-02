ba9234fa773c3cc6451f127011cdf0393a811d6b
Carlos Hernandez <hernandezcu@jbu.edu>
2022-04-02 11:21:37 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-print("Create functions that will find the largest and smallest word in the list ")
-#define text file to open
-my_file = open('words.txt', 'r')
+"""
+Your assignment is to complete the functions below.
+and then create a function that calls all of the functions and displays the results.
+"""
 
-#read text file into list 
-data = my_file.read().split("\n")
+def load_data() -> list:
+    my_file = open('words.txt', 'r')
+    data = my_file.read().split("\n")
+    return data
 
-#display content of text file
-print(type(data))
-print(data)
+def smallest_word():
+    pass
+
+def largest_word():
+    pass
+
+def total_words():
+    pass
+
+def letter_count():
+    pass
+
+def display_results():
+    pass
